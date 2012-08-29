815bbcd70a0195acc47a614d41fe32a18d7106d4
Eric Wettstein <eric@kudzudigitalcoaching.com>
2012-08-29 07:03:31 -0700

    Added widget to display "featured" member.

--- pda-members.php
+++ pda-members.php
@@ -4,7 +4,7 @@
  Plugin URI: http://www.pleasantdistrict.org/
  Version: v0.20
  Author: Serenity Investments
- Description: Show Pleasant District members.
+ Description: Display data from Pleasant District Association (pleasantdistrict.org) member site. This includes a widget and shortcodes.
  */
 
 /* not yet implemented */
@@ -23,7 +23,8 @@
 	public function PDAData ($members = false) {
 		$key =  'pdabusiness' ;
 		if ($members) $key = 'pdamember';
-
+		if( WP_DEBUG === true ) delete_transient($key);
+		
 		$this->response = get_transient($key);
 		if ($this->response === false){
 			if ($members == 'true') {
@@ -31,13 +32,13 @@
 			} else {
 				$this->response = wp_remote_get(self::BUSINESSURL );
 			}
-			if(!is_wp_error($response) ) {
+			if(!is_wp_error($this->response) ) {
 				set_transient( $key, $this->response, 60*5 );
 			} else {
+				_pdalog($this->response);
 				$this->response = null;
 			}
 		}
-		
 		if ($this->response != null) {
 			$this->json = json_decode($this->response['body']);
 		}
@@ -51,8 +52,80 @@
 		return $this->json;
 	}
 	
- 
+	/**
+	* return a random member every hour
+	**/
+	public function featured() {
+		_pdalog("getting random member");
+		$key = "pdafeatured";
+		if( WP_DEBUG === true ) delete_transient($key);
+		$featured = get_transient($key);
+		if ( $featured === false) {
+			$featured = null;
+			if (!$this->error()) {
+				$maxitems = count($this->json->items);
+				_pdalog("count: " . $maxitems);
+				$randitem = rand(0, $maxitems-1);
+				_pdalog("random: " . $randitem);
+				$featured = $this->json->items[$randitem];
+				set_transient($key, $featured, 60*60);
+			}
+		}
+		return $featured;
+	}
  }
+
+/**
+ * Adds Pleasant District Featured member widget.
+ */
+class PDA_Featured_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct() {
+		parent::__construct(
+	 		'pda_featured_widget', // Base ID
+			'PDA Featured Member', // Name
+			array( 'description' => __( 'Get random Pleasant District Association member.', 'text_domain' ), ) 
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		
+		if (empty($title)) $title = "Featured";
+		
+		echo $before_widget;
+		if ( ! empty( $title ) ) {
+			echo $before_title . $title . $after_title;
+		}
+		
+		
+		$members = new PDAData(true);
+		$featured = $members->featured();
+		echo '<div class="pda-featured">';
+		if ($featured == null ) {
+			echo "enjoy the Pleasant District";
+		} else {
+			_pdalog($featured);
+			echo '<span class="name">' . $featured->Name . '</span>';
+			echo '<span class="description">'. $featured->Description . '</span>';
+		}
+		echo '</div>';
+		echo $after_widget;
+
+	}
+}	
 
 /**
  * Handle PDA shortcodes 
@@ -73,12 +146,12 @@ class PleasantDistrictShortcodes {
 		$ret = "";
 		$ret .= "<div id=\"pda\"><dl class=\"" . $cssclass . "\">";
 		foreach ($json->items as $item) {
-			if ($item->BusinessWebSite == ''){
-				$ret .=  ("<dt>" . $item->BusinessName . "</dt>");
+			if (empty($item->WebSite) ){
+				$ret .=  ("<dt>" . $item->Name . "</dt>");
 			} else {
-				$ret .=  ("<dt><a href=\"" . $item->BusinessWebSite . "\">" . $item->BusinessName . "</a></dt>");
+				$ret .=  ("<dt><a href=\"" . $item->WebSite . "\">" . $item->Name . "</a></dt>");
 			}
-			$ret .=  "<dd><span class=\"category\">" . $item->Category . "</span> <span class=\"description\" >" . $item->BusinessDescription . "</span></dd>";
+			$ret .=  "<dd><span class=\"category\">" . $item->Category . "</span> <span class=\"description\" >" . $item->Description . "</span></dd>";
 		}
 		$ret .=  "</dl></div>";
 		return $ret;
@@ -107,10 +180,10 @@ class PleasantDistrictShortcodes {
 		foreach ($categories as $cat => $items){
 			$ret .=  "<h2>" . $cat . " <span class=\"category_count\">" . count($items) ."<span></h2><ul class=\"" . $cssclass . "\">" ;
 			foreach ($items as $item) {
-				if ($item->BusinessWebSite == ""){
-					$ret .=  "<li>" . $item->BusinessName ."</li>";
+				if ($item->WebSite == ""){
+					$ret .=  "<li>" . $item->Name ."</li>";
 				} else {
-					$ret .=  "<li><a href=\"" . $item->BusinessWebSite . "\">" . $item->BusinessName . "</a></li>";
+					$ret .=  "<li><a href=\"" . $item->WebSite . "\">" . $item->Name . "</a></li>";
 				}
 			}
 			$ret .=  "</ul>";
@@ -139,7 +212,7 @@ class PleasantDistrictShortcodes {
 			foreach ($items as $item) {
 				$current_count += 1;
 				$section_count += 1;
-				if ($item->WebSite == ""){
+				if (empty($item->WebSite)){
 					$ret .=  "<li><a href=\"http://www.pleasantdistrict.org/b" . $item->key . ".htm\">" . $item->Name . "</a></li>";
 				} else {
 					$ret .=  "<li><a href=\"" . $item->WebSite . "\">" . $item->Name . "</a></li>";
@@ -177,6 +250,7 @@ class PleasantDistrictShortcodes {
 			), $atts));
 			$ret = "";
 			
+			if ($members == '' || $members == 'no' || $members == 'false') $members = false;
 			$data = new PDAData($members);
 			if($data->error()) {
 				$ret .=  ("<div class=\"pda-error\">Error getting Pleasant District Association list.</div>");
@@ -197,4 +271,18 @@ class PleasantDistrictShortcodes {
 
 $pdacode = new PleasantDistrictShortcodes();
 add_shortcode("pleasant_district_members", array($pdacode, 'pda_members'));
+add_action( 'widgets_init', create_function( '', 'register_widget( "PDA_Featured_Widget" );' ) );
 add_action("wp_print_styles", array($pdacode, 'add_css'));
+
+
+if(!function_exists('_pdalog')){
+  function _pdalog( $message ) {
+    if( WP_DEBUG === true ){
+      if( is_array( $message ) || is_object( $message ) ){
+         print_r( $message, true );
+      } else {
+         echo $message ;
+      }
+    }
+  }
+}
