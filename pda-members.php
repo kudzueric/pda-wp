e349832efdd2db21d3b5fce589315212bded4d71
Eric Wettstein <eric@kudzudigitalcoaching.com>
2012-08-29 13:45:01 -0700

    add slogan and settings to widget
    Added options to widget settings for title and slogan. Updated the output to include slogan (call to action) and link the business name to the business website if available.

--- pda-members.php
+++ pda-members.php
@@ -102,8 +102,7 @@ class PDA_Featured_Widget extends WP_Widget {
 	public function widget( $args, $instance ) {
 		extract( $args );
 		$title = apply_filters( 'widget_title', $instance['title'] );
-		
-		if (empty($title)) $title = "Featured";
+		$slogan = apply_filters( 'widget_title', $instance['slogan'] );
 		
 		echo $before_widget;
 		if ( ! empty( $title ) ) {
@@ -115,16 +114,54 @@ class PDA_Featured_Widget extends WP_Widget {
 		$featured = $members->featured();
 		echo '<div class="pda-featured">';
 		if ($featured == null ) {
-			echo "enjoy the Pleasant District";
+			echo "<div>enjoy the Pleasant District!</div>";
 		} else {
 			_pdalog($featured);
-			echo '<span class="name">' . $featured->Name . '</span>';
-			echo '<span class="description">'. $featured->Description . '</span>';
+			echo '<div class="name">';
+			if (!empty($featured->WebSite)) echo '<a href="' . $featured->WebSite . '" title="' . $featured->Name . '">';
+			echo $featured->Name ;
+			if (!empty($featured->WebSite)) echo '</a>';
+			echo '</div>';
+			echo '<div class="description">'. $featured->Description . '</div>';
 		}
+		if (!empty($slogan)) echo '<div class="slogan"><a href="http://www.pleasantdistrict.org/" title="Pleasant District Association">' . $slogan . '</a></div>'; 
 		echo '</div>';
 		echo $after_widget;
-
 	}
+	
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['slogan'] = strip_tags( $new_instance['slogan'] );
+		return $instance;
+	}
+
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( '', 'text_domain' );
+		}
+		if ( isset( $instance[ 'slogan' ] ) ) {
+			$slogan = $instance[ 'slogan' ];
+		}
+		else {
+			$slogan = __( '', 'text_domain' );
+		}
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'slogan' ); ?>"><?php _e( 'Call to action:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'slogan' ); ?>" name="<?php echo $this->get_field_name( 'slogan' ); ?>" type="text" value="<?php echo esc_attr( $slogan ); ?>" />
+		</p>
+		<?php 
+	}
+
+
 }	
 
 /**
